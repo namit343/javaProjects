ce6729afb0924b70b8d5d150018add35f6bda55e
namit343 <75783154+namit343@users.noreply.github.com>
2020-12-10 18:48:38 +0530

    Add files via upload

---
+++ Calculator.java
@@ -0,0 +1,35 @@
+package conditionalStatements;
+import java.util.Scanner;
+
+import static java.lang.System.*;
+
+public class Calculator {
+
+	public static void main(String[] args) {
+	Scanner sc = new Scanner(in);
+
+	System.out.println("Enter First number. ");
+	int a = sc.nextInt();
+	
+	System.out.println("Enter Second number. ");
+	int b = sc.nextInt();
+	
+	System.out.println("Enter the operation. ");
+	sc.nextLine();
+	char operation = sc.nextLine().charAt(0);
+	int result = 0;
+		switch (operation) {
+			case '+' : {
+				result = a + b; break;
+			}
+
+			case '-' : result = a - b; break;
+			case '*' : result = a * b; break;
+		
+			case '/' : result = a / b; break;
+			default : System.out.println("Invalid Operation. ");
+		}
+		System.out.println("The result is:" + result );
+		}
+
+}
